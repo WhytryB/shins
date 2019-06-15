39b3d9386e1f0fb197f7b219034b94a1e01521cd
whytryb <offlittlehooligans@gmail.com>
2019-06-15 18:16:36 +0300

    update set

--- simple_frelance/settings.py
+++ simple_frelance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'af139f2e-8c75-44d2-9bdb-5670decf446e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
